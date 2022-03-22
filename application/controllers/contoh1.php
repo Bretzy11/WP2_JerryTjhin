84c9280f83b7430621146fd0d989af326d93b53e
Bretzy11 <jerrytjhin155@gmail.com>
2022-03-22 08:56:40 +0700

    menambahkan contoh1.php

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index ()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo "Nama saya Jerry Tjhin
+        Saya tinggal di daerah bambu kuning 
+        Olahraga yg saya suka adalah futsal";
+    }
+}
